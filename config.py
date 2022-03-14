29305be70c57033a2128119d76ad603d0998c58c
proggerkz <59079688+proggerkz@users.noreply.github.com>
2022-03-15 01:45:52 +0600

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+#    production token
+# TOKEN = '5243742208:AAGfVv7ld5UsBc0qSa5A-IRD0wPjox0I1OY'
+# test token
+TOKEN = '5197564576:AAHnUvhqK7800kkjYK8w0QvyzP7g65bbf3I'
+DB_USERNAME = 'db_proggerkz'
+DB_PASSWORD = 'db_orik2090'
+db_data = "mongodb+srv://" + DB_USERNAME + ":" + DB_PASSWORD + "@cluster0.zcoz3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+moderators_username = ["kh2oo"]
