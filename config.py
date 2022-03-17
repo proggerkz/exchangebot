3db71eecd7ad054807495be01e034dc8827091dc
= <proggerkz@gmail.com>
2022-03-18 05:37:35 +0600

    ASD

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 #    production token
-# TOKEN = '5143221251:AAHvhSqNzE4H5xw6-v2fuvcEOmAlT_MuDB8'
+TOKEN = '5143221251:AAHvhSqNzE4H5xw6-v2fuvcEOmAlT_MuDB8'
 # test token
-TOKEN = '5197564576:AAHnUvhqK7800kkjYK8w0QvyzP7g65bbf3I'
+# TOKEN = '5197564576:AAHnUvhqK7800kkjYK8w0QvyzP7g65bbf3I'
 DB_USERNAME = 'db_proggerkz'
 DB_PASSWORD = 'db_orik2090'
 db_data = "mongodb+srv://" + DB_USERNAME + ":" + DB_PASSWORD + "@cluster0.zcoz3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
