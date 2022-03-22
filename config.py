9918ed4a2ea6f5d934d16805a41ff13d9eb7a229
proggerkz <59079688+proggerkz@users.noreply.github.com>
2022-03-22 11:46:53 +0600

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-#    production token
-TOKEN = '5143221251:AAHvhSqNzE4H5xw6-v2fuvcEOmAlT_MuDB8'
-# test token
-# TOKEN = '5197564576:AAHnUvhqK7800kkjYK8w0QvyzP7g65bbf3I'
-DB_USERNAME = 'db_proggerkz'
-DB_PASSWORD = 'db_orik2090'
-db_data = "mongodb+srv://" + DB_USERNAME + ":" + DB_PASSWORD + "@cluster0.zcoz3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-moderators_username = ["kh2oo"]
-
